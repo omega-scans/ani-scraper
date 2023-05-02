e7965ba6f8104d69d9673561548dda89aeb0ac9a
omega-scans <omegascanlation@gmail.com>
2023-05-02 20:45:22 +0530

    init

---
+++ cache_purge_jsdeliver.py
@@ -0,0 +1,17 @@
+import requests
+
+urls = [
+    "https://cdn.jsdelivr.net/gh/omega-scans/ani-scrape@main/static/css/anime.min.css",
+    "https://cdn.jsdelivr.net/gh/omega-scans/ani-scrape@main/static/css/episode.min.css",
+    "https://cdn.jsdelivr.net/gh/omega-scans/ani-scrape@main/static/css/home.min.css",
+    "https://cdn.jsdelivr.net/gh/omega-scans/ani-scrape@main/static/css/search.min.css",
+    "https://cdn.jsdelivr.net/gh/omega-scans/ani-scrape@main/video.css",
+    "https://cdn.jsdelivr.net/gh/omega-scans/ani-scrape@main/static/js/home.min.js",
+    "https://cdn.jsdelivr.net/gh/omega-scans/ani-scrape@main/static/js/player.min.js",
+]
+
+
+for url in urls:
+    url = url[24:]
+    r = requests.get("https://purge.jsdelivr.net" + url)
+    print(r.json().get("status"), url)
